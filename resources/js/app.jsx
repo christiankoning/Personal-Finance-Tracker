e805c805539ad80794bf6c1bfbdaf992be428a7a
ChristianKoning <S1170814@student.windesheim.nl>
2024-12-31 11:25:34 +0100

    Add React

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import './bootstrap';
+
+import React from "react"
+import ReactDOM from 'react-dom/client';
+
+import Home from './Home';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Home />
+);
