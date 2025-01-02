f24b8b3a17c5a8470a5ad23698000b3005992159
ChristianKoning <S1170814@student.windesheim.nl>
2025-01-02 13:35:24 +0100

    Add transaction routing

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TransactionController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('web')->group(function () {
@@ -15,4 +16,10 @@ Route::middleware('web')->group(function () {
     })->middleware('auth');
     Route::post('/reset-password', [AuthController::class, 'sendPasswordResetLink']);
     Route::post('/reset-password/confirm', [AuthController::class, 'resetPassword']);
+});
+Route::middleware(['auth', 'web'])->group(function () {
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::put('/transactions/{id}', [TransactionController::class, 'update']);
+    Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
 });
